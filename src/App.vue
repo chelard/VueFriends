9995f41e1857ee20a8212f649206271b242d4255
Helard Flores <helard@gmail.com>
2022-12-14 11:02:12 -0500

    se hizo el uso de props and emits

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,55 @@
 <template>
   <section>
-    <h2>My friends</h2>
+    <header><h1>My friends</h1></header>
     <ul>
-      <li v-for="friend in friends" :key="friend.id">
-        <h3>{{ friend.name }}</h3>
-        <p>Age: {{ friend.age }}</p>
-        <p>Email: {{ friend.email }}</p>
-        <p>Phone: {{ friend.phone }}</p>
-      </li>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :age="friend.age"
+        :email-address="friend.email"
+        :phone-number="friend.phone"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavorite"
+      ></friend-contact>
     </ul>
   </section>
-  
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       friends: [
-        { 
-          id:'Helard',
-          name: 'Helard Flores',
+        {
+          id: "Helard",
+          name: "Helard Flores",
           age: 30,
-          email: 'john@gmail.com',
-          phone: '555-555-5555'
+          email: "john@gmail.com",
+          phone: "555-555-5555",
+          isFavorite: true,
         },
-        { 
-          id:'John',
-          name: 'John Doe',
+        {
+          id: "John",
+          name: "John Doe",
           age: 25,
-          email: 'doe@gmail.com',
-          phone: '555-555-5555'
-        }],
+          email: "doe@gmail.com",
+          phone: "555-555-5555",
+          isFavorite: false,
+        },
+      ],
     };
   },
-  components: {
-    
-  }
+  methods: {
+    toggleFavorite(friendId) {
+      const friendIndex = this.friends.find((friend) => friend.id === friendId);
 
-  
-}
+      friendIndex.isFavorite = !friendIndex.isFavorite;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/styles.css"></style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,61 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(favorite)" : "" }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li>Age: {{ age }}</li>
+      <li>Email: {{ emailAddress }}</li>
+      <li>Phone: {{ phoneNumber }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {  
+  props:{
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: true,
+      default: false,      
+      },
+    },
+  
+  data() {
+    return {
+      detailsAreVisible: false,   
+      
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+      
+    },
+
+  },
+};
+</script>
